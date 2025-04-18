0f7a295f23313a1311e5958f5f4dfadaa19291b8
Hazeera-Hashim <hazeerahashim3112@gmail.com>
2025-04-18 23:55:56 +0500

    Create Branch_Unit.v

---
+++ 1.Single Cycle/srcs/Branch_Unit.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/18/2025 03:39:59 AM
+// Design Name: 
+// Module Name: Branch_Unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Branch_Unit(Funct3, ReadData1, ReadData2, sel);
+    input [2:0] Funct3;
+    input [63:0] ReadData1, ReadData2;
+    output reg sel;
+    
+    initial begin
+          sel = 1'b0;
+        end
+      always @(*) begin
+          case (Funct3)
+            3'b000: begin //beq
+                sel= ReadData1 == ReadData2? 1'b1:1'b0;
+                end
+             3'b100:begin //blt
+                  sel=ReadData1 < ReadData2? 1'b1:1'b0;
+                end
+            3'b101: begin //bgt
+                sel= ReadData1 > ReadData2? 1'b1:1'b0;
+              end    
+          endcase
+         end
+endmodule
