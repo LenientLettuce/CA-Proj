320ed4f3dcd42bf1d8972fec2b6f152a7e86848a
Hazeera-Hashim <hazeerahashim3112@gmail.com>
2025-04-19 11:59:47 +0500

    Update ALU_64_bit.v

--- 1.Single Cycle/srcs/ALU_64_bit.v
+++ 1.Single Cycle/srcs/ALU_64_bit.v
@@ -2,20 +2,19 @@ module ALU_64_bit(
     input [63:0] a,         // Operand A
     input [63:0] b,         // Operand B
     input [3:0] ALUOp,      // ALU Operation code
-    output reg ZERO,        // Zero flag
-    output reg [63:0] Result // Computed result
+    output reg [63:0] Result, // Computed result
+    output ZERO        // Zero flag
 );
     
     wire [63:0] and_r, or_r, xor_r, add, sub; 
     
-    // Logical operations
+    // Operations
     assign and_r = a & b;
     assign or_r  = a | b;
     assign xor_r = a ^ b;
-    
-    // Arithmetic operations
-    assign add = a + b;      // Addition
-    assign sub = a - b;      // Subtraction
+    assign add = a + b;      
+    assign sub = a - b;      
+    assign slli =  a * (2 ** b);
 
     always @(*) begin
         case (ALUOp)
@@ -24,11 +23,9 @@ module ALU_64_bit(
             4'b0010: Result = add;   // ADD
             4'b0011: Result = sub;   // SUB
             4'b0100: Result = xor_r; // XOR
+            4'b0101: Result = slli;  // SLLI
             default: Result = 64'b0; // Default to zero
         endcase
-
-        // Set flags
-        ZERO = (Result == 64'b0);  
-        //Cout = (ALUOp == 4'b0010) ? (a > add) : 1'b0; // Carry only for ADD
     end
-endmodule
+           assign ZERO = Result? 0:1; 
+endmodule 
