d84120a47f7dfcfb7ba9ea19f54613e17a0bbb36
umercdkee <146094120+umercdkee@users.noreply.github.com>
2025-04-21 05:29:00 +0500

    Update Program_Counter_3.v

--- 3.Hazard Detect + Forwading/srcs/Program_Counter_3.v
+++ 3.Hazard Detect + Forwading/srcs/Program_Counter_3.v
@@ -7,13 +7,11 @@ module Program_Counter_3(
     output reg [63:0] PC_out
 
 );
-    always @ (posedge clk)  // Triggered on rising clock edge
-    begin
-        if (reset == 1'b1)  // Reset condition
-           PC_out <= 64'b0;
-        else if (PC_Write == 1'b0)
-            PC_out <= PC_in; // Update PC value
-        else 
-            PC_out = PC_in;
-    end
+    always @(posedge clk) begin
+    if (reset)
+        PC_out <= 64'b0;
+    else if (PC_Write)
+        PC_out <= PC_in;  // Only update when PC_Write is 1
+end
+
 endmodule
