c17260f8ecbb2db6df67799db1b1f8254f7f1cf9
umercdkee <146094120+umercdkee@users.noreply.github.com>
2025-04-21 05:29:45 +0500

    Update IF_ID_3.v

--- 3.Hazard Detect + Forwading/srcs/IF_ID_3.v
+++ 3.Hazard Detect + Forwading/srcs/IF_ID_3.v
@@ -1,30 +1,7 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/19/2025 11:40:39 PM
-// Design Name: 
-// Module Name: IF_ID
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
-`timescale 1ns / 1ps
+timescale 1ns / 1ps
 
 module IF_ID_3(
-    input clk, reset, flush, IF_ID_Write, 
+    input clk, reset, IF_ID_Write, 
     input [63:0] PC_out,
     input [31:0] Instruction, 
     output reg [63:0] IF_ID_PC_out, 
@@ -36,10 +13,6 @@ module IF_ID_3(
                 IF_ID_Instruction <= 0; 
                 IF_ID_PC_out <= 0;
             end
-        else if (flush) begin
-            // Branch flush - clear instructions but keep PC
-            IF_ID_Instruction <= 32'b0;
-        end
         else if (IF_ID_Write == 0) //for nop bcs of load
             begin
                 IF_ID_PC_out <= IF_ID_PC_out; 
