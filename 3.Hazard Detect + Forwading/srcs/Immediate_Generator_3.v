fbd425a46f9f7f018b8d6d75a39ff035af9c02b4
umercdkee <146094120+umercdkee@users.noreply.github.com>
2025-04-21 05:28:17 +0500

    Update Immediate_Generator_3.v

--- 3.Hazard Detect + Forwading/srcs/Immediate_Generator_3.v
+++ 3.Hazard Detect + Forwading/srcs/Immediate_Generator_3.v
@@ -1,33 +1,31 @@
-`timescale 1ns / 1ps
-
 module Immediate_Generator_3(
     input [31:0] instruction,
     output reg [63:0] immediate
 );
+    wire [12:0] imm_SB;
+    wire [11:0] imm_I;
+    wire [11:0] imm_S;
+    wire [20:0] imm_J;
+
+    assign imm_SB = {instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+    assign imm_I = instruction[31:20];
+    assign imm_S = {instruction[31:25], instruction[11:7]};
+    assign imm_J = {instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+
     always @(*) begin
         case (instruction[6:0])
-            7'b1100011: begin //SB type
-                immediate = {{51{instruction[31]}}, 
-                           instruction[31], 
-                           instruction[7], 
-                           instruction[30:25], 
-                           instruction[11:8], 
-                           1'b0};
-            end
-            7'b0000011, 7'b0010011, 7'b1100111: begin // I-type (load, addi, jalr)
-                immediate = {{52{instruction[31]}}, instruction[31:20]};
-            end
-            7'b0100011: begin // S-type (store)
-                immediate = {{52{instruction[31]}}, instruction[31:25], instruction[11:7]};
-            end
-            7'b1101111: begin // J-type (jal)
-                immediate = {{44{instruction[31]}}, instruction[31], instruction[19:12], instruction[20],
-                             instruction[30:21]};
-            end
-            7'b0110111, 7'b0010111: begin // U-type (lui, auipc)
+            7'b1100011: // SB-type (e.g., beq)
+                immediate = {{51{imm_SB[12]}}, imm_SB};
+            7'b0000011, 7'b0010011, 7'b1100111: // I-type
+                immediate = {{52{imm_I[11]}}, imm_I};
+            7'b0100011: // S-type
+                immediate = {{52{imm_S[11]}}, imm_S};
+            7'b1101111: // J-type (jal)
+                immediate = {{43{instruction[31]}}, imm_J};
+            7'b0110111, 7'b0010111: // U-type (lui, auipc)
                 immediate = {instruction[31:12], 12'b0};
-            end
-            default: immediate = 64'd0;
+            default:
+                immediate = 64'd0;
         endcase
     end
 endmodule
